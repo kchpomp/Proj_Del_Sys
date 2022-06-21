5f0c5d9bb12dc06ae1868ea0e39cfaa91b84d326
kchpomp <70205946+kchpomp@users.noreply.github.com>
2022-06-22 01:52:24 +0300

    Add files via upload

--- session/session.go
+++ session/session.go
@@ -1,40 +1,51 @@
 package session
 
-import (
-	"utils"
-)
+import "github.com/kchpomp/Proj_Del_Sys/utils"
 
+//Structure for storing username
 type sessionData struct {
 	Username string
+	password string
 }
 
+//Structure to store a data in map with string key
 type Session struct {
 	data map[string]*sessionData
 }
 
+//Session object
 func NewSession() *Session {
+	// Object for new session
 	s := new(Session)
 
+	//Create not nil object
 	s.data = make(map[string]*sessionData)
 
+	//return object
 	return s
 }
 
-func (s *Session) Init(username string) string {
+//Session initialization method that receives username and password
+func (s *Session) Init(username string, password string) string {
+	//Generate session ID using utils
 	sessionId := utils.GenerateId()
 
-	data := &sessionData{Username: username}
+	//create data that will be stored in session
+	data := &sessionData{Username: username, password: password}
+
+	//write data in session ID
 	s.data[sessionId] = data
 
+	//return session ID
 	return sessionId
 }
 
-func (s *Session) Get(sessionId string) string {
+func (s *Session) Get(sessionId string) (string, string) {
 	data := s.data[sessionId]
 
 	if data == nil {
-		return ""
+		return "", ""
 	}
 
-	return data.Username
+	return data.Username, data.password
 }
